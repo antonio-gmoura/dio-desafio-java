6491273c56a4a24bdd3c8591b43bb61b4b78692b
Antonio Moura <antonio.gmoura@gmail.com>
2022-01-28 23:52:47 -0400

    Inclui Desafio2Saida1

---
+++ src/Desafio2Saida1.java
@@ -0,0 +1,11 @@
+public class Desafio2Saida1 {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("|                                     |");
+        System.out.println("---------------------------------------");
+    }
+}
